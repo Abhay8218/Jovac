9594a949f3ab310fddf26e5ddfd9e61db145b792
Abhay8218 <as9886072@gmail.com>
2026-07-06 14:43:05 +0530

    Full navbar

---
+++ Sample_navBar/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer =()=>{
+    return(
+        <footer>
+            <p>About us</p>
+            <p>Contact us</p>
+            <p>Legal</p>
+        </footer>
+    );
+}
+export default Footer;
